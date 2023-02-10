90d06bee84572214919382fa2c87c1664ca78d17
guilhrib <ribeiro.guilhermedesouza@gmail.com>
2023-02-10 18:52:18 -0300

    refactor: organize the application in modules

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
-import { CategoryRepository } from "../repositories/CategoryRepository"
-import { CreateCategoryService } from "../services/CreateCategoryService"
+import { CategoryRepository } from "../modules/cars/repositories/CategoryRepository"
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService"
 
 const categoriesRoutes = Router()
 const categoryRepository = new CategoryRepository()
