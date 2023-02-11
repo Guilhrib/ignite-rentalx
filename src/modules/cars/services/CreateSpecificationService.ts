6ecfa97c287bb7ddaa4e6f5a8db39a01ef07fa71
guilhrib <ribeiro.guilhermedesouza@gmail.com>
2023-02-11 00:03:15 -0300

    feat: specification resource

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,5 +1,24 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+  name: string,
+  description: string
+}
+
 class CreateSpecificationService {
-  execute() {}
+  constructor(private specificationRepository: ISpecificationRepository) {}
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExist = this.specificationRepository.findById(name)
+
+    if (specificationAlreadyExist) {
+      throw new Error('Specification already exists!')
+    }
+
+    this.specificationRepository.create({
+      name,
+      description
+    })
+  }
 }
 
 export { CreateSpecificationService }
---
+++ src/routes/specification.routes.ts
@@ -0,0 +1,19 @@
+import { response, Router } from "express";
+import { SpecificationRepository } from "../modules/cars/repositories/SpecificationRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationRoutes = Router()
+
+const specificationRepository = new SpecificationRepository()
+
+specificationRoutes.post('/', (req, res) => {
+  const { name, description } = req.body
+
+  const createSpecificationService = new CreateSpecificationService(specificationRepository)
+
+  createSpecificationService.execute({ name, description })
+
+  return res.status(201).send()
+})
+
+export { specificationRoutes }
