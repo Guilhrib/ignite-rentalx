44e8e74d59b74b2e057906b1c4c9673245d6f90d
Guilhrib <ribeiro.guilhermedesouza@gmail.com>
2023-07-26 23:03:15 -0300

    chore: add typeorm and postgres driver

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import swaggerFile from './swagger.json'
 import { categoriesRoutes } from "./routes/categories.routes" 
 import { specificationRoutes } from "./routes/specification.routes"
 
+import './database'
+
 const app = express()
 
 app.use(express.json())
