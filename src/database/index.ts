4920aa11ac6d8a21c4d1f3b782f3864fed7651b4
Guilhrib <ribeiro.guilhermedesouza@gmail.com>
2023-07-28 00:13:47 -0300

    feat: implement migrations with typeorm

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,17 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import { DataSource } from "typeorm";
 
-interface IOptions {
-  host: string;
-}
-
-getConnectionOptions().then(options => {
-  const newOptions = options as IOptions;
-  newOptions.host = 'database'
-  createConnection({
-    ...options
-  })
+export const myAppDataSource = new DataSource({
+  type: "postgres",
+  port: 5432,
+  host: "postgres",
+  username: "postgres",
+  password: "admin",
+  database: "rentx",
+  migrations: [
+    "src/database/migrations/*.ts"
+  ]
 })
+
+myAppDataSource.initialize()
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err))
